1215f1c90ae3076f98fb625ba11377d63b9284a4
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2023-08-29 07:23:27 +0200

    bug: fixed numCPUs

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ if (cluster.isPrimary && configuration.maxCPU !== 1) {
   loggerService.log(`Primary ${process.pid} is running`);
 
   // Fork workers.
-  for (let i = 1; i < 2; i++) {
+  for (let i = 1; i < numCPUs; i++) {
     cluster.fork();
   }
 
