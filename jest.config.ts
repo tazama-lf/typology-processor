66f19af763d79cb87f4de6e651395e4f504a5363
rtkay123 <70331483+rtkay123@users.noreply.github.com>
2023-08-29 12:18:55 +0200

    refactor: use lib apm service (#59)
    * refactor: use lib apm service
    
    * refactor: use lib apm service
    
    ---------
    
    Co-authored-by: rtkay123 <rodney.kanjala@sybrin.com>

--- jest.config.ts
+++ jest.config.ts
@@ -58,7 +58,7 @@ const config: Config.InitialOptions = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 73,
+      branches: 67,
       functions: 95,
       lines: 95,
       statements: 95,
--- src/apm.ts
+++ src/apm.ts
@@ -1,14 +1,14 @@
-import apm from 'elastic-apm-node';
+import { Apm } from '@frmscoe/frms-coe-lib/lib/services/apm';
 import { configuration } from './config';
 /*
  * Initialize the APM Logging
  **/
-if (configuration.apm.active === 'true') {
-  apm.start({
-    serviceName: configuration.apm?.serviceName,
-    secretToken: configuration.apm?.secretToken,
-    serverUrl: configuration.apm?.url,
-    usePathAsTransactionName: true,
-    active: Boolean(configuration.apm?.active),
-  });
-}
+const apm = new Apm({
+  serviceName: configuration.apm?.serviceName,
+  secretToken: configuration.apm?.secretToken,
+  serverUrl: configuration.apm?.url,
+  usePathAsTransactionName: true,
+  active: Boolean(configuration.apm?.active),
+});
+
+export default apm;
