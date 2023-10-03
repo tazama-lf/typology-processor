cb1f4593882b8752c8a9b8c97609de59e66fae29
Len Bekker <len.bekker@sybrin.com>
2023-10-03 17:35:57 +0200

    fix: update deps, remove cluster, remove unused

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,46 @@
 ARG BUILD_IMAGE=oven/bun
-ARG RUN_IMAGE=oven/bun
+ARG RUN_IMAGE=oven/bun:slim
 
+# BUILD
 FROM ${BUILD_IMAGE} AS builder
 LABEL stage=build
+
 WORKDIR /home/app
 COPY ./src ./src
 COPY ./package*.json ./
 COPY ./tsconfig.json ./
-# COPY ./.env.production ./
 COPY bunfig.toml ./
+
 ARG GH_TOKEN
 RUN sed -i "s/\${GH_TOKEN}/$GH_TOKEN/g" ./bunfig.toml
 
 RUN bun install
 
-# Turn down the verbosity to default level.
-ENV NPM_CONFIG_LOGLEVEL warn
+# RUN
+FROM ${RUN_IMAGE} AS run-env
+LABEL stage=run
 
-ENV mode="http"
-ENV upstream_url="http://127.0.0.1:3000"
-ENV prefix_logs="false"
+USER bun
+
+WORKDIR /home/app
+COPY --from=builder /home/app/node_modules ./node_modules
+COPY ./src ./src
+COPY ./package*.json ./
+COPY ./tsconfig.json ./
+COPY bunfig.toml ./
+
+# APP
 ENV FUNCTION_NAME=typology-processor-rel-1-0-0
 ENV NODE_ENV=production
-ENV CMS_ENDPOINT=
-ENV CACHE_ENABLED=
 
-#Redis
+# REDIS
 ENV CACHE_TTL=30
 ENV REDIS_DB=0
 ENV REDIS_AUTH=
 ENV REDIS_SERVERS=
 ENV REDIS_IS_CLUSTER=
 
-#Nats
+# NATS
 ENV STARTUP_TYPE=nats
 ENV PRODUCER_STREAM=
 ENV CONSUMER_STREAM=
@@ -42,7 +50,7 @@ ENV ACK_POLICY=Explicit
 ENV PRODUCER_STORAGE=File
 ENV PRODUCER_RETENTION_POLICY=Workqueue
 
-#Database
+# DATABASE
 ENV DATABASE_NAME=Configuration
 ENV DATABASE_URL=
 ENV DATABASE_USER=root
@@ -50,20 +58,19 @@ ENV DATABASE_PASSWORD=
 ENV DATABASE_CERT_PATH=
 ENV COLLECTION_NAME=typologyExpression
 
-# Apm
+# APM
 ENV APM_ACTIVE=true
 ENV APM_SERVICE_NAME=typology-processor
 ENV APM_URL=http://apm-server.development.svc.cluster.local:8200/
 ENV APM_SECRET_TOKEN=
 
-# Logstash
+# LOGSTASH
 ENV LOGSTASH_HOST=logstash.development.svc.cluster.local
 ENV LOGSTASH_PORT=8080
 ENV LOGSTASH_LEVEL='info'
 
-# Set healthcheck command
+# CONTAINER
 HEALTHCHECK --interval=60s CMD [ -e /tmp/.lock ] || exit 1
 EXPOSE 4222
 
-# Execute watchdog command
-CMD ["bun", "src/index.ts"]
+CMD ["bun", "./src/index.ts"]
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import './apm';
 import { LoggerService, type DatabaseManagerInstance } from '@frmscoe/frms-coe-lib';
 import { StartupFactory, type IStartupService } from '@frmscoe/frms-coe-startup-lib';
-import cluster from 'cluster';
-import os from 'os';
 import { configuration } from './config';
 import { handleTransaction } from './logic.service';
 import { Singleton } from './services/services';
@@ -63,36 +61,18 @@ process.on('unhandledRejection', (err) => {
   loggerService.error(`process on unhandledRejection error: ${JSON.stringify(err) ?? '[NoMetaData]'}`);
 });
 
-const numCPUs = os.cpus().length > configuration.maxCPU ? configuration.maxCPU + 1 : os.cpus().length + 1;
-
-if (cluster.isPrimary && configuration.maxCPU !== 1) {
-  loggerService.log(`Primary ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 1; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    loggerService.log(`worker ${Number(worker.process.pid)} died, starting another worker`);
-    cluster.fork();
-  });
-} else {
-  // Workers can share any TCP connection
-  // In this case it is an NATS server
-  (async () => {
-    try {
-      if (configuration.env !== 'test') {
-        await dbInit();
-        await runServer();
-        loggerService.log(`Worker ${process.pid} started`);
-      }
-    } catch (err) {
-      loggerService.error(`Error while starting services on Worker ${process.pid}`, err);
-      process.exit(1);
+(async () => {
+  try {
+    if (configuration.env !== 'test') {
+      await dbInit();
+      await runServer();
+      loggerService.log(`typology-processor[${process.pid}] started`);
     }
-  })();
-  loggerService.log(`Worker ${process.pid} started`);
-}
+  } catch (err) {
+    loggerService.error(`Error while starting services on Worker ${process.pid}`, err);
+    process.exit(1);
+  }
+})();
+
 
 export { databaseManager };
