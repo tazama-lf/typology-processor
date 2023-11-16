0bf7b91f56df3c02aa4c7652f974b66dccdaea56
cshezi <cshezi@sybrin.co.za>
2023-11-16 16:26:17 +0200

    fix: reduce r/w of redis by control list

--- src/logic.service.ts
+++ src/logic.service.ts
@@ -8,14 +8,13 @@ import { type TypologyResult } from '@frmscoe/frms-coe-lib/lib/interfaces/proces
 import { type CADPRequest } from '@frmscoe/frms-coe-lib/lib/interfaces/processor-files/CADPRequest';
 import { configuration } from './config';
 import { type IExpression, type IRuleValue, type ITypologyExpression } from './interfaces/iTypologyExpression';
-import { type MetaData } from './interfaces/metaData';
+// import { type MetaData } from './interfaces/metaData';
+import fs from 'fs';
 
-const calculateDuration = (startTime: bigint): number => {
-  const endTime = process.hrtime.bigint();
-  return Number(endTime - startTime);
-};
-
-const noDescription = 'No description provided in typology config.';
+// const calculateDuration = (startTime: bigint): number => {
+//   const endTime = process.hrtime.bigint();
+//   return Number(endTime - startTime);
+// };
 
 const evaluateTypologyExpression = (ruleValues: IRuleValue[], ruleResults: RuleResult[], typologyExpression: IExpression): number => {
   let toReturn = 0.0;
@@ -80,119 +79,264 @@ const evaluateTypologyExpression = (ruleValues: IRuleValue[], ruleResults: RuleR
   return toReturn;
 };
 
-const executeRequest = async (
-  transaction: any,
-  typology: Typology,
-  ruleResult: RuleResult,
-  ruleId: string,
-  networkMap: NetworkMap,
-  channelHost: string,
-  metaData: MetaData,
-): Promise<void> => {
-  const startTime = process.hrtime.bigint();
+// const executeRequest = async (
+//   transaction: any,
+//   typology: Typology,
+//   ruleResult: RuleResult,
+//   ruleId: string,
+//   networkMap: NetworkMap,
+//   channelHost: string,
+//   metaData: MetaData,
+// ): Promise<void> => {
+//   const startTime = process.hrtime.bigint();
 
-  const typologyResult: TypologyResult = {
-    result: 0.0,
-    id: typology.id,
-    cfg: typology.cfg,
-    prcgTm: 0,
-    ruleResults: [],
-    workflow: {
-      alertThreshold: 0,
-    },
-  };
+//   const typologyResult: TypologyResult = {
+//     result: 0.0,
+//     id: typology.id,
+//     cfg: typology.cfg,
+//     prcgTm: 0,
+//     ruleResults: [],
+//     workflow: {
+//       alertThreshold: 0,
+//     },
+//   };
 
-  const cadpReqBody: CADPRequest = {
-    typologyResult,
-    transaction,
-    networkMap,
-  };
-  const spanExecReq = apm.startSpan(`${typologyResult.id}.exec.Req`);
+//   const cadpReqBody: CADPRequest = {
+//     typologyResult,
+//     transaction,
+//     networkMap,
+//   };
+//   const spanExecReq = apm.startSpan(`${typologyResult.id}.exec.Req`);
 
-  try {
-    const transactionType = 'FIToFIPmtSts';
-    const transactionID = transaction[transactionType].GrpHdr.MsgId;
-    const cacheKey = `TP_${transactionID}_${typology.id}_${typology.cfg}`;
-    const jruleResultsCount = await databaseManager.addOneGetCount(`${cacheKey}`, { ruleResult: { ...ruleResult } });
+//   try {
+//     const transactionType = 'FIToFIPmtSts';
+//     const transactionID = transaction[transactionType].GrpHdr.MsgId;
+//     const cacheKey = `TP_${transactionID}_${typology.id}_${typology.cfg}`;
+//     const jruleResultsCount = await databaseManager.addOneGetCount(`${cacheKey}`, { ruleResult: { ...ruleResult } });
 
-    if (jruleResultsCount && jruleResultsCount < typology.rules.length) {
-      typologyResult.prcgTm = calculateDuration(startTime);
-      spanExecReq?.end();
-      return;
+//     if (jruleResultsCount && jruleResultsCount < typology.rules.length) {
+//       typologyResult.prcgTm = calculateDuration(startTime);
+//       spanExecReq?.end();
+//       return;
+//     }
+
+//     const jruleResults = await databaseManager.getMemberValues(`${cacheKey}`);
+//     const ruleResults: RuleResult[] = jruleResults.map((res) => res.ruleResult as RuleResult);
+
+//     cadpReqBody.typologyResult.ruleResults = ruleResults;
+
+//     const expressionRes = (await databaseManager.getTypologyExpression(typology)) as unknown[][];
+//     if (!expressionRes?.[0]?.[0]) {
+//       loggerService.warn(`No Typology Expression found for Typology ${typology.id}@${typology.cfg}`);
+//       typologyResult.prcgTm = calculateDuration(startTime);
+//       spanExecReq?.end();
+//       return;
+//     }
+
+//     const expression = expressionRes[0][0] as ITypologyExpression;
+//     const span = apm.startSpan(`[${transactionID}] eval.typology.expr`);
+//     const typologyResultValue = evaluateTypologyExpression(expression.rules, ruleResults, expression.expression);
+//     span?.end();
+
+//     typologyResult.result = typologyResultValue;
+//     typologyResult.prcgTm = calculateDuration(startTime);
+//     typologyResult.review = false;
+//     if (expression.workflow.interdictionThreshold)
+//       typologyResult.workflow.interdictionThreshold = expression.workflow.interdictionThreshold;
+//     if (expression.workflow.alertThreshold) typologyResult.workflow.alertThreshold = expression.workflow.alertThreshold;
+
+//     // Interdiction
+//     // Send Result to CMS
+//     if (expression.workflow.interdictionThreshold && typologyResultValue >= expression.workflow.interdictionThreshold) {
+//       const spanCms = apm.startSpan(`[${transactionID}] Send Typology result to CMS`);
+//       typologyResult.review = true;
+//       server
+//         .handleResponse({ ...cadpReqBody, metaData }, [configuration.cmsProducer])
+//         .catch((error) => {
+//           loggerService.error(`Error while sending Typology result to CMS`, error as Error);
+//         })
+//         .finally(() => {
+//           spanCms?.end();
+//         });
+//     }
+
+//     if (!expression.workflow.alertThreshold) {
+//       loggerService.error(`Typology ${typology.cfg} config missing alert Threshold`);
+//     } else if (typologyResultValue >= expression.workflow.alertThreshold) {
+//       loggerService.log(`Typology ${typology.cfg} alerting on transaction : ${transactionID} with a trigger of: ${typologyResultValue}`);
+//       typologyResult.review = true;
+//     }
+//     cadpReqBody.typologyResult = typologyResult;
+
+//     // Send TADP request with this Typology's result
+//     const spanTadpr = apm.startSpan(`[${transactionID}] Send Typology result to TADP`);
+//     server
+//       .handleResponse({ ...cadpReqBody, metaData })
+//       .catch((error) => {
+//         loggerService.error(`Error while sending Typology result to TADP`, error as Error);
+//       })
+//       .finally(() => {
+//         spanTadpr?.end();
+//       });
+
+//     const spanDelete = apm.startSpan(`cache.delete.[${transactionID}].Typology interim cache key`);
+//     await databaseManager.deleteKey(cacheKey);
+//     spanDelete?.end();
+//   } catch (error) {
+//     loggerService.error(`Failed to process Typology ${typology.id} request`, error as Error, 'executeRequest');
+//   } finally {
+//     loggerService.log(`Concluded processing of Rule ${ruleId}`);
+//     spanExecReq?.end();
+//   }
+// };
+
+interface UniqueIdentifiers {
+  rules: { ids: string[]; total: number };
+  typologies: { ids: string[]; total: number };
+}
+
+const getUniqueRulesCount = (networkMap: NetworkMap): UniqueIdentifiers => {
+  const uniqueIdsRules: string[] = [];
+  const uniqueIdsTypologies: string[] = [];
+
+  for (const channel of networkMap.messages[0].channels) {
+    for (const typology of channel.typologies) {
+      if (!uniqueIdsTypologies.includes(typology.id)) {
+        uniqueIdsTypologies.push(typology.id);
+      }
+      for (const rule of typology.rules) {
+        if (!uniqueIdsRules.includes(rule.id)) {
+          uniqueIdsRules.push(rule.id);
+        }
+      }
     }
-
-    const jruleResults = await databaseManager.getMemberValues(`${cacheKey}`);
-    const ruleResults: RuleResult[] = jruleResults.map((res) => res.ruleResult as RuleResult);
-
-    cadpReqBody.typologyResult.ruleResults = ruleResults;
-
-    const expressionRes = (await databaseManager.getTypologyExpression(typology)) as unknown[][];
-    if (!expressionRes?.[0]?.[0]) {
-      loggerService.warn(`No Typology Expression found for Typology ${typology.id}@${typology.cfg}`);
-      typologyResult.prcgTm = calculateDuration(startTime);
-      spanExecReq?.end();
-      return;
-    }
-
-    const expression = expressionRes[0][0] as ITypologyExpression;
-    const span = apm.startSpan(`[${transactionID}] eval.typology.expr`);
-    const typologyResultValue = evaluateTypologyExpression(expression.rules, ruleResults, expression.expression);
-    span?.end();
-
-    typologyResult.result = typologyResultValue;
-    typologyResult.prcgTm = calculateDuration(startTime);
-    typologyResult.review = false;
-    if (expression.workflow.interdictionThreshold)
-      typologyResult.workflow.interdictionThreshold = expression.workflow.interdictionThreshold;
-    if (expression.workflow.alertThreshold) typologyResult.workflow.alertThreshold = expression.workflow.alertThreshold;
-
-    // Interdiction
-    // Send Result to CMS
-    if (expression.workflow.interdictionThreshold && typologyResultValue >= expression.workflow.interdictionThreshold) {
-      const spanCms = apm.startSpan(`[${transactionID}] Send Typology result to CMS`);
-      typologyResult.review = true;
-      server
-        .handleResponse({ ...cadpReqBody, metaData }, [configuration.cmsProducer])
-        .catch((error) => {
-          loggerService.error(`Error while sending Typology result to CMS`, error as Error);
-        })
-        .finally(() => {
-          spanCms?.end();
-        });
-    }
-
-    if (!expression.workflow.alertThreshold) {
-      loggerService.error(`Typology ${typology.cfg} config missing alert Threshold`);
-    } else if (typologyResultValue >= expression.workflow.alertThreshold) {
-      loggerService.log(`Typology ${typology.cfg} alerting on transaction : ${transactionID} with a trigger of: ${typologyResultValue}`);
-      typologyResult.review = true;
-    }
-    cadpReqBody.typologyResult = typologyResult;
-
-    // Send TADP request with this Typology's result
-    const spanTadpr = apm.startSpan(`[${transactionID}] Send Typology result to TADP`);
-    server
-      .handleResponse({ ...cadpReqBody, metaData })
-      .catch((error) => {
-        loggerService.error(`Error while sending Typology result to TADP`, error as Error);
-      })
-      .finally(() => {
-        spanTadpr?.end();
-      });
-
-    const spanDelete = apm.startSpan(`cache.delete.[${transactionID}].Typology interim cache key`);
-    await databaseManager.deleteKey(cacheKey);
-    spanDelete?.end();
-  } catch (error) {
-    loggerService.error(`Failed to process Typology ${typology.id} request`, error as Error, 'executeRequest');
-  } finally {
-    loggerService.log(`Concluded processing of Rule ${ruleId}`);
-    spanExecReq?.end();
   }
+  return {
+    rules: { ids: uniqueIdsRules, total: uniqueIdsRules.length },
+    typologies: { ids: uniqueIdsTypologies, total: uniqueIdsTypologies.length },
+  };
+};
+
+const saveToRedisGetAll = async (transaction: any, ruleResult: RuleResult): Promise<RuleResult[] | undefined> => {
+  const transactionType = 'FIToFIPmtSts';
+  const transactionID = transaction[transactionType].GrpHdr.MsgId;
+  const cacheKey = `TP_${transactionID}`;
+
+  const currentlyStoredRuleResult = await databaseManager.addOneGetAll(cacheKey, { ruleResult: { ...ruleResult } });
+  const ruleResults: RuleResult[] | undefined = currentlyStoredRuleResult.map((res) => res.ruleResult as RuleResult);
+  loggerService.log(JSON.stringify(currentlyStoredRuleResult));
+
+  return ruleResults;
+};
+
+const ruleResultAggregation = (networkMap: NetworkMap, ruleList: RuleResult[]): TypologyResult[] => {
+  const typologyResult: TypologyResult[] = [];
+
+  networkMap.messages.forEach((message) => {
+    message.channels.forEach((channel) => {
+      channel.typologies.forEach((typology) => {
+        typologyResult.push({
+          id: typology.id,
+          cfg: typology.cfg,
+          result: -1,
+          ruleResults: ruleList.filter((rRule) => typology.rules.some((tRule) => rRule.id === tRule.id)),
+          workflow: { alertThreshold: -1 },
+        });
+      });
+    });
+  });
+
+  return typologyResult;
+};
+
+const evaluateTypologySendRequest = async (
+  typologyResults: TypologyResult[],
+  networkMap: NetworkMap,
+  transaction: any,
+): Promise<CADPRequest | undefined> => {
+  let cadpReqBody: CADPRequest = { networkMap, transaction, typologyResult: typologyResults[0] };
+  for (let index = 0; index < typologyResults.length; index++) {
+    if (typologyResults[index].result === -1) {
+      const expressionRes = (await databaseManager.getTypologyExpression({
+        id: typologyResults[index].id,
+        cfg: typologyResults[index].cfg,
+        host: '',
+        desc: '',
+        rules: [],
+      })) as unknown[][];
+
+      if (!expressionRes?.[0]?.[0]) {
+        loggerService.warn(`No Typology Expression found for Typology ${typologyResults[index].id}@${typologyResults[index].cfg}`);
+        return {
+          typologyResult: typologyResults[index],
+          transaction,
+          networkMap,
+        };
+      }
+
+      const expression = expressionRes[0][0] as ITypologyExpression;
+      const typologyResultValue = evaluateTypologyExpression(expression.rules, typologyResults[index].ruleResults, expression.expression);
+
+      typologyResults[index].result = typologyResultValue;
+
+      if (expression.workflow.interdictionThreshold)
+        typologyResults[index].workflow.interdictionThreshold = expression.workflow.interdictionThreshold;
+
+      if (expression.workflow.alertThreshold) typologyResults[index].workflow.alertThreshold = expression.workflow.alertThreshold;
+
+      typologyResults[index].review = false;
+
+      if (!expression.workflow.alertThreshold) {
+        loggerService.error(`Typology ${typologyResults[index].cfg} config missing alert Threshold`);
+      } else if (typologyResultValue >= expression.workflow.alertThreshold) {
+        loggerService.log(`Typology ${typologyResults[index].cfg} alerting on transaction : with a trigger of: ${typologyResultValue}`);
+        typologyResults[index].review = true;
+      }
+
+      cadpReqBody = {
+        typologyResult: typologyResults[index],
+        transaction,
+        networkMap,
+      };
+
+      if (networkMap.messages[0].channels[0].typologies[index].rules.length <= typologyResults[index].ruleResults.length) {
+        if (expression.workflow.interdictionThreshold && typologyResultValue >= expression.workflow.interdictionThreshold) {
+          typologyResults[index].review = true;
+          server
+            .handleResponse({ ...cadpReqBody }, [configuration.cmsProducer])
+            .catch((error) => {
+              loggerService.error(`Error while sending Typology result to CMS`, error as Error);
+            })
+            .finally(() => {
+              /* Empty Final */
+            });
+        }
+
+        if (!expression.workflow.alertThreshold) {
+          loggerService.error(`Typology ${typologyResults[index].cfg} config missing alert Threshold`);
+        } else if (typologyResultValue >= expression.workflow.alertThreshold) {
+          loggerService.log(`Typology ${typologyResults[index].cfg} alerting on transaction :  with a trigger of: ${typologyResultValue}`);
+          typologyResults[index].review = true;
+        }
+        cadpReqBody.typologyResult = typologyResults[index];
+        loggerService.log(`Last rule on the transaction sending to TADP`);
+        fs.writeFileSync('./result.json', JSON.stringify({ ...cadpReqBody }));
+        server
+          .handleResponse({ ...cadpReqBody })
+          .catch((error) => {
+            loggerService.error(`Error while sending Typology result to TADP`, error as Error);
+          })
+          .finally(() => {
+            /* Empty Final */
+          });
+      }
+    }
+  }
+  return cadpReqBody;
 };
 
 export const handleTransaction = async (transaction: any): Promise<void> => {
-  let typologyCounter = 0;
+  loggerService.log(`Started Handing Typology work`);
   const metaData = transaction.metaData;
   loggerService.log(`traceParent in typroc: ${JSON.stringify(metaData?.traceParent)}`);
   const apmTransaction = apm.startTransaction('typroc.handleTransaction', {
@@ -200,33 +344,57 @@ export const handleTransaction = async (transaction: any): Promise<void> => {
   });
   const networkMap: NetworkMap = transaction.networkMap;
   const ruleResult: RuleResult = transaction.ruleResult;
-
   const parsedTrans = transaction.transaction;
 
-  const requests = [];
+  // const requests = [];
 
-  const spanCadpRes = apm.startSpan('cadproc.sendReq');
-  for (const channel of networkMap.messages[0].channels) {
-    for (const typology of channel.typologies.filter((typo) => typo.rules.some((r) => r.id === ruleResult.id))) {
-      // will loop through every Typology here
-      typologyCounter++;
-      const channelHost = channel.host;
+  // const spanCadpRes = apm.startSpan('cadproc.sendReq');
 
-      requests.push(
-        executeRequest(parsedTrans, typology, ruleResult, ruleResult.id, networkMap, channelHost, {
-          ...metaData,
-          traceParent: apm.getCurrentTraceparent(),
-        }),
-      );
-    }
+  // Rules list creation and saving to Redis #Step 1
+  loggerService.log(`Got ${ruleResult.id} rule result`);
+
+  // Save the rules Result to Redis and continue with the available #Step 1
+  const rulesList: RuleResult[] | undefined = await saveToRedisGetAll(parsedTrans, ruleResult);
+
+  if (!rulesList) {
+    loggerService.log('Redis records should never be undefined');
+    return;
   }
 
-  await Promise.all(requests);
-  spanCadpRes?.end();
+  // Aggregations typology config merge with rule result #Step 2
+  const jtypologyResults: TypologyResult[] = ruleResultAggregation(networkMap, rulesList);
 
-  const transactionType = 'FIToFIPmtSts';
-  const transactionID = parsedTrans[transactionType].GrpHdr.MsgId;
-  const result = `${typologyCounter} typologies initiated for transaction ID: ${transactionID}`;
-  loggerService.log(`${result} for Rule ${ruleResult.id}`);
-  apmTransaction?.end();
+  // Typology evaluation and Send to TADP Step 3
+  const typologyResults = await evaluateTypologySendRequest(jtypologyResults, networkMap, parsedTrans);
+
+  // ---------------------------------------------------------------------------------------
+  // for (const channel of networkMap.messages[0].channels) {
+  //   for (const typology of channel.typologies.filter((typo) => typo.rules.some((r) => r.id === ruleResult.id))) {
+  //     // will loop through every Typology here
+
+  //     const channelHost = channel.host;
+
+  //     requests.push(
+  //       executeRequest(parsedTrans, typology, ruleResult, ruleResult.id, networkMap, channelHost, {
+  //         ...metaData,
+  //         traceParent: apm.getCurrentTraceparent(),
+  //       }),
+  //     );
+  //   }
+  // }
+
+  // await Promise.all(requests);
+  // spanCadpRes?.end();
+
+  // const transactionType = 'FIToFIPmtSts';
+  // const transactionID = parsedTrans[transactionType].GrpHdr.MsgId;
+  // const result = `${typologyCounter} typologies initiated for transaction ID: ${transactionID}`;
+  // loggerService.log(`${result} for Rule ${ruleResult.id}`);
+  const uniqueCountNetworkMap = getUniqueRulesCount(networkMap);
+  if (rulesList.length >= uniqueCountNetworkMap.rules.total) {
+    apmTransaction?.end();
+    fs.writeFileSync('./test.json', JSON.stringify(typologyResults));
+  }
+
+  //
 };
